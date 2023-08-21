f1f3053581010d9ef6cb0b05d7b6210f3234c2fa
san <chingistsyrenov0@gmail.com>
2023-08-21 18:20:43 +0800

    experimented with input methods

---
+++ day1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	// "os"
+)
+
+func panicOnErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// data, err := os.ReadFile("input.txt")
+	// panicOnErr(err)
+
+}
